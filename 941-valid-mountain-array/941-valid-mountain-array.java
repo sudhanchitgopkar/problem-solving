b93430f961d60aad2f0b6afbc876d8df27503082
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-03 15:30:53 -0500

    Time: 1 ms (100.00%), Space: 39.5 MB (97.46%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if (arr.length < 3) return false;
+        
+        int l = 0;
+        int r = arr.length-1;
+        
+        while (arr[l] < arr[l+1])
+            if (++l == arr.length-1)
+                return false;
+        
+        while (arr[r] < arr[r-1])
+            if (--r == 0)
+                return false;
+        
+        if (l == r) return true;
+        else return false;
+        
+    } //validMountain
+} //solution
