e096a485e4d6f303d69870610536860d92ad92aa
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-06 20:15:29 -0500

    Time: 1 ms, Memory: 39.9 MB - LeetHub

---
+++ fizz-buzz/fizz-buzz.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List <String> sol = new ArrayList <String> ();
+        int i = 1;
+        
+        while (i <= n) {
+            if (i % 3 == 0 && i % 5 == 0)
+                sol.add("FizzBuzz");
+            else if (i % 3 == 0)
+                sol.add("Fizz");
+            else if (i % 5 == 0)
+                sol.add("Buzz");
+            else
+                sol.add(Integer.toString(i));
+            i++;
+        } //while
+        
+        return sol;
+    } //fizzbuzz
+} //Solution
