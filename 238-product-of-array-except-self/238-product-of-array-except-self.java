0510a752eee65cdb5df8c146cc0fefc00ecec5ce
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-09 12:28:52 -0500

    Time: 4 ms (14.53%), Space: 56.2 MB (37.66%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,33 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int [] sol = new int [nums.length];
         int [] pre = new int [nums.length];
         int [] post = new int [nums.length];
+        int [] sol = new int [nums.length];
         
         pre[0] = nums[0];
-        post[post.length-1] = nums[nums.length-1];
-        
         for (int i = 1; i < pre.length; i++) 
-            pre[i] = pre[i-1]*nums[i];
+            pre[i] = nums[i] * pre[i-1];
         
-        for (int i = post.length-2; i >= 0; i--) 
-            post[i] = post[i+1] * nums[i];
+        post[post.length-1] = nums[nums.length-1];
+         for (int i = post.length-2; i >= 0; i--) 
+            post[i] = nums[i] * post[i+1];
         
-        for (int i = 0; i< sol.length; i++) {
-            if (i == 0)
-                sol[0] = post[1];
-            else if (i == sol.length-1)
-                sol[i] = pre[i-1];
-            else
-                sol[i] = pre[i-1] * post[i+1];
-        } //for
+        sol[0] = post[1];
+        sol[sol.length-1] = pre[post.length-2];
+        for (int i = 1; i < post.length-1; i++)
+            sol[i] = pre[i-1] * post[i+1];
         
-        
-        for (int i:pre)
-            System.out.print(i + " ");
-        System.out.println();
-        for (int i:post)
-            System.out.print(i + " ");
-            return sol;
+        return sol;
     } //productExcpetSelf
 } //Solution
