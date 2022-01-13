6aafcffeefdf7de59601841448fd4dd73dc96f68
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-12 22:10:00 -0500

    Time: 17 ms (94.29%), Space: 42.7 MB (82.72%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(nums);
         
         for (int i = 0; i < nums.length-2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+            if (i == 0 || nums[i] != nums[i-1]) {
                 int l = i+1, r = nums.length-1, target = 0-nums[i];
                 while (r > l) {
                     if (nums[l] + nums[r] > target)
@@ -14,12 +14,9 @@ class Solution {
                         l++;
                     else {
                         sol.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                        while (r > l && nums[l] == nums[l+1])
-                            l++;
-                         while (r > l && nums[r] == nums[r-1])
-                            r--;
-                        l++;
-                        r--;
+                        while (r > l && nums[l] == nums[l+1]) l++;
+                        while (r > l && nums[r] == nums[r-1]) r--;
+                        l++; r--;
                     } //if
                 } //while
             } //if
