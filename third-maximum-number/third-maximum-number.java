a77725713ad3d4a227ff72c80971752b0280a352
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-13 23:57:26 -0500

    Time: 5 ms, Memory: 40.7 MB - LeetHub

--- third-maximum-number/third-maximum-number.java
+++ third-maximum-number/third-maximum-number.java
@@ -4,19 +4,21 @@ class Solution {
 
         for (Integer n : nums) {
             if (!n.equals(max1) && !n.equals(max2) && !n.equals(max3)) {
-            if (max1 == null || n > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = n;
-            } else if (max2 == null || n > max2) {
-                max3 = max2;
-                max2 = n;
-            } else if (max3 == null || n > max3) {
-                max3 = n;
-            }
-        }
-        }
-        return max3 == null ? max1 : max3;
-    
+                if (max1 == null || n > max1) {
+                    max3 = max2;
+                    max2 = max1;
+                    max1 = n;
+                } else if (max2 == null || n > max2) {
+                    max3 = max2;
+                    max2 = n;
+                } else if (max3 == null || n > max3)  {
+                    max3 = n;
+                } //if
+            } //if
+        }  //for
+        if (max3 == null)
+            return max1;
+        else
+            return max3;
     } //thirdMax
 } //Solution
