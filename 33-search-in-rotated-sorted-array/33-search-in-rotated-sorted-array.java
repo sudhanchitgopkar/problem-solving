daa28a73982a25fa5f203ddec7759280c5843f7c
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-14 18:47:43 -0500

    Time: 1 ms (34.93%), Space: 39.3 MB (28.58%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -8,7 +8,6 @@ class Solution {
     
             if (nums[m] == target)
                 return m;
-            
             if (nums[l] <= nums[m] && nums[l] <= target && nums[m] >= target)
                 r = m-1;
             else if (nums[l] <= nums[m])
