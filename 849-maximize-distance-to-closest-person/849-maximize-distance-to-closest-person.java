6c5df546fca5bb9c927cf93b093e95c80a1482f6
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-16 00:27:55 -0500

    Time: 2 ms (77.56%), Space: 41.3 MB (47.43%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
@@ -4,6 +4,30 @@ class Solution {
         int lMax = 0, gMax = 0;
         
         while (r < seats.length) {
+            if (seats[r] == 1 && seats[l] == 0) {
+                lMax = r-l;
+                l = r++;
+            } else if (seats[r] == 1) {
+                lMax = (r-l)/2;
+                l = r++;
+            } else {
+                r++;
+            } //if
+            gMax = lMax > gMax ? lMax : gMax;
+        } //while
+        
+        if (seats[l] == 1 && --r-l > gMax)
+            return r-l;
+        else
+            return gMax;
+    } //maxDistToClosest
+} //Solution
+
+/*
+        int l = 0, r = 0;
+        int lMax = 0, gMax = 0;
+        
+        while (r < seats.length) {
             if (seats[r] == 1) {
                 if (seats[l] == 0)
                     lMax = r-l;
@@ -20,5 +44,4 @@ class Solution {
             return r-l;
         else
             return gMax;
-    } //maxDistToClosest
-} //Solution
+*/
