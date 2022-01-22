4f3bf360461411f886bc1dab7177ed93f8c73c42
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-22 13:04:31 -0500

    Time: 3 ms (23.94%), Space: 90 MB (43.91%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -3,11 +3,11 @@ class Solution {
         int lMax = nums[0], gMax = nums[0];
         
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > lMax + nums[i])
-                lMax = nums[i];
+            if (nums[i] > lMax + nums[i]) lMax = nums[i];
             else lMax += nums[i];
             gMax = lMax > gMax ? lMax : gMax;
         } //for
+        
         return gMax;
     } //maxSubArr
 } //Solution
