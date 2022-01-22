9e83e92a45cfe3d7780b160f91c3ca4e44c554ee
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-22 12:12:59 -0500

    Time: 1 ms (95.59%), Space: 39.2 MB (72.15%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int n : nums)
+            xor = xor ^ n;
+        return xor;
+    } //singleNumber
+} //Solution
