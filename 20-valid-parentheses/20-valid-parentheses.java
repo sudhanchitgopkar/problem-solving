584b96a4bb071ddcc322fbc93c439467047ca356
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-24 22:25:02 -0500

    Time: 3 ms (43.95%), Space: 39 MB (11.92%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,29 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack <Character> paren = new Stack <Character> ();
-
-        for (int i = 0; i < s.length(); i++) {            
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
-                paren.push(s.charAt(i));
-            else {
-                if (paren.isEmpty())
-                    return false;
-                if (s.charAt(i) == ')' && paren.pop() != '(')
-                    return false;
-                if (s.charAt(i) == '}' && paren.pop() != '{')
-                    return false;
-                if (s.charAt(i) == ']' && paren.pop() != '[')
-                    return false;
-            } //if
-
+        Stack <Character> c = new Stack <Character> ();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '{') c.push('}');
+            else if (s.charAt(i) == '[') c.push(']');
+            else if (s.charAt(i) == '(') c.push(')');
+            
+            if (s.charAt(i) == '}' || s.charAt(i) == ']' || s.charAt(i) == ')' )
+                if (c.isEmpty()) return false;
+                else if (s.charAt(i) != c.pop()) return false; 
         } //for
         
-        if (paren.isEmpty())
-            return true;
-        else
-            return false;
-        
-
-        
+        if (c.isEmpty()) return true;
+        else return false;
     } //isValid
 } //Solution
