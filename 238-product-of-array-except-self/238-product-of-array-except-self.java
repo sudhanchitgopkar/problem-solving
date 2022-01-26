2e3ace7840424bb7dfeabde64af83d098e910390
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-25 23:36:33 -0500

    Time: 4 ms (21.65%), Space: 57.1 MB (18.62%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -7,16 +7,17 @@ class Solution {
         l[0] = nums[0];
         r[nums.length-1] = nums[nums.length-1];
         
-        for (int i = 1; i < nums.length; i++) 
-            l[i] = l[i-1] * nums[i];   
+        for (int i = 1; i < nums.length; i++)
+            l[i] = l[i-1] * nums[i];
+        
         for (int i = nums.length-2; i >= 0; i--)
             r[i] = r[i+1] * nums[i];
         
-        sol[0]  = r[1]; sol[nums.length-1] = l[nums.length-2];
+        sol[0] = r[1];
+        sol[nums.length-1] = l[nums.length-2];
         
-        for (int i = 1; i < nums.length-1; i++) {
+        for (int i = 1; i < nums.length-1; i++)
             sol[i] = l[i-1] * r[i+1];
-        } //for
         
         return sol;
     } //productExcpetSelf
