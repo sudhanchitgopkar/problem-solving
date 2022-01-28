cf65abdbae9972fd6a21dc2a39440bfdbf74c069
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-01-27 23:59:05 -0500

    Time: 1388 ms (5.00%), Space: 47.3 MB (8.12%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -5,7 +5,7 @@ class Solution {
         
         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         
-        for (int i = 0; i < s.length()/2; ++i) 
+        for (int i = 0; i < s.length()/2; i++) 
             if (s.charAt(i) != s.charAt(s.length() - 1 - i))
                 return false;
         return true;
