6c7c715946a26d02d745dc0aa44e35b880346ec9
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-05-18 18:16:01 -0400

    Time: 4 ms (77.81%), Space: 43.5 MB (59.55%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,22 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap <Character, Integer> freq = new HashMap <Character, Integer> ();
+        int [] freq = new int [26];
+        
+        if (s.length() != t.length()) return false;
         
         for (int i = 0; i < s.length(); i++) {
-            if (freq.containsKey(s.charAt(i)))
-                freq.put(s.charAt(i), freq.get(s.charAt(i)) + 1);
-            else freq.put(s.charAt(i), 1);
-        } //for
-                  
-        for (int i = 0; i < t.length(); i++) {
-            if (freq.containsKey(t.charAt(i)))
-                freq.put(t.charAt(i), freq.get(t.charAt(i)) - 1);
-            else return false;
-        } //for
-                  
-        for (Integer i : freq.values())
-            if (i != 0) return false;
-                  
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        
+        for (int f:freq) if (f != 0) return false;
         return true;
+        
     } //isAnagram
 } //Solution
