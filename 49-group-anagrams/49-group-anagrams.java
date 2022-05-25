1f5542d2b9dd1741dd19764f9bc8e497ad8b7939
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-05-25 13:55:56 -0700

    Time: 12 ms (58.85%), Space: 55.9 MB (44.75%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int [] freq = new int [26];
+        
         HashMap <String, List<String>> ana = new HashMap <String, List<String>> ();
         
-        for (String s : strs) {
-            char [] ca = s.toCharArray();
-            Arrays.sort(ca);
-            String key = String.valueOf(ca);
-            
-            if (ana.containsKey(key))
-                ana.get(key).add(s);
-            else {
-                ana.put(key, new ArrayList<String>());
-                ana.get(key).add(s);
-            } //if
+        for (String s: strs){
+            char [] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+            if (!ana.containsKey(key)) ana.put(key, new ArrayList<String>());
+            ana.get(key).add(s);                         
         } //for
         
         return new ArrayList<List<String>>(ana.values());
     } //groupAnagrams
 } //Sol
+
+/*
+1. str -> sorted char array: use to tell if ana with other str
+2. check if ana already exists
+    a. if yes, add str to existing ana list
+    b. if no, start new ana list with str
+3. package up all ana lists into one list
+*/
