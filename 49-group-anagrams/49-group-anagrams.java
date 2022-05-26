054c372129b0b41ea80f797581793242c1d2acea
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-05-25 20:59:50 -0700

    Time: 9 ms (82.24%), Space: 55.2 MB (67.99%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,19 +1,30 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap <String, List<String>> ana = new HashMap <String, List<String>> ();
+        HashMap<String,ArrayList<String>> ana = new HashMap<String,ArrayList<String>>();
         
         for (String s : strs) {
             char [] ca = s.toCharArray();
             Arrays.sort(ca);
             String key = String.valueOf(ca);
             
-            if (!ana.containsKey(key))
-                ana.put(key, new ArrayList<String>());
-            
+           
+            if (!ana.containsKey(key)) 
+                ana.put(key,new ArrayList<String>());
             ana.get(key).add(s);
         } //for
-       
+        
         return new ArrayList<List<String>> (ana.values());
     } //groupAnagrams
 } //Sol
 
+/*
+1. convert each str -> its sorted char string representation X
+2. pair anagrams together X
+3. return paired anagrams
+
+
+eat -> aet
+tea -> aet
+
+cat -> act
+*/
