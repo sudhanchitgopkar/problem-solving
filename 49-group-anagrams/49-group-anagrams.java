c4fd008829efc34fb95f078d3577faa462c6c253
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-05-25 21:12:20 -0700

    Time: 10 ms (77.52%), Space: 55.4 MB (63.65%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,16 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,ArrayList<String>> ana = new HashMap<String,ArrayList<String>>();
+        HashMap <String, ArrayList<String>> ana = new HashMap <String, ArrayList<String>> ();
         
         for (String s : strs) {
             char [] ca = s.toCharArray();
             Arrays.sort(ca);
             String key = String.valueOf(ca);
             
-           
-            if (!ana.containsKey(key)) 
-                ana.put(key,new ArrayList<String>());
-            ana.get(key).add(s);
+            if (ana.containsKey(key)) {
+                ana.get(key).add(s);
+            } else {
+                ana.put(key, new ArrayList<String>());
+                ana.get(key).add(s);
+            } //if
         } //for
         
         return new ArrayList<List<String>> (ana.values());
@@ -18,13 +20,14 @@ class Solution {
 } //Sol
 
 /*
-1. convert each str -> its sorted char string representation X
-2. pair anagrams together X
-3. return paired anagrams
+1. convert each str -> sorted char representation X
+2. group all sets of anagrams X
+3. return our set of sets
+*/
 
+/*
+eat -> a,e,t -> aet
+tea -> a,e,t -> aet
 
-eat -> aet
-tea -> aet
-
-cat -> act
+bat -> a,b,t -> abt
 */
