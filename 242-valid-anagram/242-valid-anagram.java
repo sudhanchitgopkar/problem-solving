d14b76731186b92053ce721dfd3bfef0f2696b35
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-05-26 13:23:48 -0700

    Time: 7 ms (49.75%), Space: 43.8 MB (50.19%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,16 +1,29 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         int [] freq = new int [26];
-        
         if (s.length() != t.length()) return false;
         
         for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
             freq[t.charAt(i) - 'a']--;
-        }
+        } //for
         
-        for (int f:freq) if (f != 0) return false;
+        for (int f : freq)
+            if (f != 0) return false;
+        
         return true;
-        
     } //isAnagram
 } //Solution
+
+
+/*
+s = "abc" : incremented 
+t = "ca" : decremented
+
+26 unique letters
+int array of length 26
+
+ a b c d e
+|0|0|0|0|0|...
+
+*/
