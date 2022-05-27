cf381e518473032b2c15a0edb61b730374a0b65a
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-05-26 20:51:22 -0700

    Time: 27 ms (13.48%), Space: 49.1 MB (7.49%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet <String> seen = new HashSet <String> ();
+        
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board.length; c++) {
+                if (board[r][c] != '.') {
+                    if (seen.contains("row" + r + board[r][c]) || 
+                    seen.contains("col" + c + board[r][c]) ||
+                    seen.contains("sq" + r/3 + "," + c/3 + board[r][c])) {
+                        return false;
+                    } else {
+                        seen.add("row" + r + board[r][c]);
+                        seen.add("col" + c + board[r][c]);
+                        seen.add("sq" + r/3 + "," + c/3 + board[r][c]);
+                        } //if
+                } //if
+                } //for
+        } //for
+        return true;
+    } //isValidSudoku
+} //Solution
