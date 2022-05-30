6b8f0bd99ec403cd1c542f35846c9cfb4a7a9790
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-05-29 17:49:41 -0700

    Time: 38 ms (42.39%), Space: 59.2 MB (42.82%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,25 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> sol = new LinkedList<>();
-        
         Arrays.sort(nums);
+        List<List<Integer>> sol = new ArrayList<List<Integer>> ();
         
-        for (int i = 0; i < nums.length-2; i++) {
-            if (i == 0 || nums[i] != nums[i-1]) {
-                int l = i+1, r = nums.length-1, target = 0-nums[i];
-                while (r > l) {
-                    if (nums[l] + nums[r] > target)
-                        r--;
-                    else if (nums[l] + nums[r] < target)
+        for (int i = 0; i < nums.length-1; i++) {
+            if (i == 0 || nums[i-1] != nums[i]) {
+               int l = i+1, r = nums.length-1;
+                while (l < r) {
+                    if (nums[l] + nums[r] + nums[i] < 0) {
                         l++;
-                    else {
-                        sol.add(Arrays.asList(nums[i],nums[l],nums[r]));
-                        while (r > l && nums[l] == nums[l+1]) l++;
-                        while (r > l && nums[r] == nums[r-1]) r--;
-                        l++; r--;
+                    } else if (nums[l] + nums[r] + nums[i] > 0) { 
+                        r--;
+                    } else {
+                        ArrayList<Integer> temp = new ArrayList<Integer> ();
+                        temp.add(nums[l++]); temp.add(nums[r--]); temp.add(nums[i]);
+                        sol.add(temp);
+                        while (r > l && nums[r] == nums[r+1]) r--;
+                        while (r > l && nums[l] == nums[l-1]) l++;
                     } //if
                 } //while
-            } //if
+            } //if    
         } //for
         
         return sol;
