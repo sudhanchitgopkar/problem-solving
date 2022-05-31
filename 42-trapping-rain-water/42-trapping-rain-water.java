a2f502efdb20ac3e6f570e32dffaffae18821958
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-05-30 21:51:55 -0700

    Time: 1 ms (95.23%), Space: 48.8 MB (10.07%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -6,9 +6,6 @@ class Solution {
         int lMax = l, rMax = r;
         int trapped = 0;
         
-        //while (height[l] <= height[l+1]) l++;
-        //while (height[r] <= height[r-1]) r--;
-        
         while (l < r) {
             if (height[lMax] < height[rMax]) {
                 if (height[l] > height[lMax]) lMax = l;
