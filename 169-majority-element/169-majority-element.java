0daf32f8792e009cf65d88a5efeec8203baea3aa
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-06-02 22:15:28 -0700

    Time: 21 ms (12.96%), Space: 55.3 MB (65.35%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,13 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap <Integer, Integer> freq = new HashMap <Integer, Integer> ();
+        HashMap <Integer, Integer> h = new HashMap <Integer, Integer> ();
         
-        for (int n:nums) 
-            if (freq.containsKey(n))
-                if (freq.get(n)+1 >= nums.length/2+1) return n;
-                else freq.put(n, freq.get(n)+1);
-            else freq.put(n,1);
+        for (int n : nums) {
+            h.put(n,h.getOrDefault(n,0)+1);
+            if (h.get(n) > nums.length/2) return n;
+        } //for
         
-        return nums[0];
+        return -1; //unreachable!
     } //majorityElem
 } //Solution
