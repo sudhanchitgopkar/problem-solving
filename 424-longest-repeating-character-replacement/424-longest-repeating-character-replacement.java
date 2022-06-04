c1507a3175fca01b216ba575764d21768f6a31bc
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-06-04 14:39:34 -0700

    Time: 17 ms (49.39%), Space: 42.8 MB (66.49%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,20 +1,37 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int l = 0, maxLen = 0, maxCount = 0;
+        int l = 0;
+        int lMax = 0, gMax = 0, maxFreq = 0;
         int [] freq = new int [26];
         
         for (int r = 0; r < s.length(); r++) {
-            freq[s.charAt(r) - 'A']++;
-            maxCount = Math.max(maxCount, freq[s.charAt(r) - 'A']);
+            freq[s.charAt(r)  - 'A']++;
+            if (freq[s.charAt(r)  - 'A'] > maxFreq) maxFreq = freq[s.charAt(r)  - 'A'];
             
-            while (r - l + 1 - maxCount > k) {
-                freq[s.charAt(l) - 'A']--;
-                l++;
-            } //while
+            if (r - l + 1 - maxFreq <= k) {
+                //string is valid
+            } else {
+                while (r - l + 1 - maxFreq > k) {
+                    freq[s.charAt(l)-'A']--;
+                    l++;
+                    maxFreq = update(freq);
+                } //while
+            } //if
             
-            maxLen = Math.max(maxLen, r-l+1);
+            lMax = r - l + 1;
+            
+            if (gMax < lMax) gMax = lMax;
         } //for
         
-        return maxLen;
+        return gMax;
     } //charReplacement
+    
+    public int update (int [] freq) {
+        int max = freq[0];
+        
+        for (int f : freq)
+            if (max < f) max = f;
+        
+        return max;
+    } //update
 } //Sol
