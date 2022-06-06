3b21f4688e3ebcc0fb0334a48633f83044179bfb
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-06-05 17:42:40 -0700

    Time: 11 ms (79.08%), Space: 51.2 MB (18.92%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+        
+        int [] tfreq = new int [128];
+        int [] window = new int [128];
+        int l = 0;
+        int lMin = Integer.MAX_VALUE, gMin = Integer.MAX_VALUE;
+        int matches = 0;
+        String sol = "";
+        
+        //populate tFreq
+        for (int i = 0; i < t.length(); i++)
+            tfreq[t.charAt(i)]++;
+        
+        //populate window
+        for (int i = 0; i < t.length() - 1; i++) {
+            window[s.charAt(i)]++;
+            if (window[s.charAt(i)] <= tfreq[s.charAt(i)]) matches++;
+        } //for
+        
+        //main
+        for (int r = t.length() - 1; r < s.length(); r++) {
+            window[s.charAt(r)]++;
+            if (window[s.charAt(r)] <= tfreq[s.charAt(r)]) matches++;
+            
+            while (matches == t.length()) {
+                lMin = r - l + 1;
+                if (lMin < gMin) {
+                    gMin = lMin;
+                    sol = s.substring(l,r+1);
+                } //if
+                window[s.charAt(l)]--;
+                if (window[s.charAt(l)] < tfreq[s.charAt(l)]) matches--;
+                l++;
+            } //while
+        } //for
+        
+        return sol;
+    } //minWin
+} //Sol
