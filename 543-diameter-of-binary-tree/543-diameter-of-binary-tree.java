1c1734bd6ec97bc54c27b7d42534e4184227c854
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-06-25 22:24:17 -0700

    Time: 0 ms (100.00%), Space: 42 MB (91.24%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -25,7 +25,7 @@ class Solution {
         if (root == null) return 0;
         int l = recurse(root.left);
         int r = recurse(root.right);
-        if (l + r > max) max = l + r;
+        max = Math.max(max, l + r);
         return 1 + Math.max(l,r);
     } //recurse
 } //Sol
