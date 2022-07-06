8682366ea93b4ca63c500a3c6a36548ea8570dbc
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-07-05 21:37:49 -0700

    Time: 32 ms (7.59%), Space: 44.4 MB (66.69%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -5,21 +5,21 @@ class Solution {
     
     private int quickSelect (int [] nums, int l, int r, int k) {
         int pivot = l;
-        for (int curr = l; curr < r; curr++) {
-            if (nums[curr] <= nums[r]) {
-                int temp = nums[curr];
-                nums[curr] = nums[pivot];
-                nums[pivot++] = temp;
-            }
-        } //for
         
-        int temp = nums[pivot];
-        nums[pivot] = nums[r];
-        nums[r] = temp;
+        for (int curr = l; curr < r; curr++) 
+            if (nums[curr] <= nums[r]) 
+                swap(nums,curr,pivot++);
+        
+        swap(nums,pivot,r);
         
         if (pivot == k) return nums[pivot];
         else if (pivot > k) return quickSelect(nums,0,pivot-1,k);
         return quickSelect(nums,pivot+1,nums.length-1,k);
     } //quickSelect
     
+    private void swap (int [] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    } //swap   
 } //Sol
