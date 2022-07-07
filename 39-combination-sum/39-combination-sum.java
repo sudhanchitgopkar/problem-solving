78d746f264dcc3f2bc9c8610f7e7c56cecac012e
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-07-06 22:58:20 -0700

    Time: 9 ms (33.41%), Space: 45.1 MB (54.31%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,25 +1,27 @@
 class Solution {
     List<List<Integer>> sol;
+    int t;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         sol = new ArrayList<List<Integer>>();
-        backtrack(candidates,new ArrayList<Integer>(),0,target,0);
+        t = target;
+        backtrack(candidates,new ArrayList<Integer>(),0,0);
         return sol;
     } //combinationSum
     
-    private void backtrack (int [] candidates, List<Integer> subset, int curr, int target, int sum) {
+    private void backtrack (int [] candidates, List<Integer> subset, int curr, int sum) {
        
-        if (curr >= candidates.length || sum > target || sol.size() >= 150) {
+        if (curr >= candidates.length || sum > t || sol.size() >= 150) {
             return;
-        } else if (sum == target) {
+        } else if (sum == t) {
             sol.add(new ArrayList<Integer>(subset));
             return;
         } //if
         
         subset.add(candidates[curr]);
-        backtrack(candidates,subset,curr,target,sum+candidates[curr]);
+        backtrack(candidates,subset,curr,sum+candidates[curr]);
         
         subset.remove(subset.size()-1);
-        backtrack(candidates,subset,curr+1,target,sum);
+        backtrack(candidates,subset,curr+1,sum);
         
     } //backtrack
 } //Sol
