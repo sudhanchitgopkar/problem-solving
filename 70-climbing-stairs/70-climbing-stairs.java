a5d99d46ff6db4c32ca3dc8d489a92af55d28b1d
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-07-17 22:25:30 -0700

    Time: 0 ms (100.00%), Space: 41 MB (33.22%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,7 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
         if (n == 1) return 1;
-        if (n == 2) return 2;
         
         int prev = 1, prevprev = 1, curr = 0;
         
