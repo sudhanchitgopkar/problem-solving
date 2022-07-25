d09ddeaeb70b37f72712a5c6b9f58d67baffd7c9
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-07-24 22:17:49 -0700

    Time: 1 ms (53.06%), Space: 40.5 MB (74.56%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,44 +1,16 @@
 class Solution {
-    int rowLen, colLen;
     public int uniquePaths(int m, int n) {
-        if (m == 1 || n == 1) return 1;
         int [][] numPaths = new int [m][n];
-        numPaths[m-1][n-1] = 1;
-        rowLen = m;
-        colLen = n;
-        findPath(0,0,numPaths);
-        return numPaths[0][1] + numPaths[1][0];
+        
+        for (int row = 0; row < m; row++)
+            numPaths[row][0] = 1;
+        for (int col = 0; col < n; col++)
+            numPaths[0][col] = 1;
+        
+        for (int row = 1; row < m; row++)
+            for (int col = 1; col < n; col++)
+                numPaths[row][col] = numPaths[row-1][col] + numPaths[row][col-1];
+        
+        return numPaths[m-1][n-1];
     } //uniquePaths
-    
-    private int findPath (int row, int col, int [][] numPaths) {
-        if (row == rowLen-1 && col == colLen - 1) return 1;
-        if (row >= rowLen || col >= colLen) return 0;
-        
-        if (row + 1 >= rowLen) {
-            if (numPaths[row][col+1] == 0) {
-                numPaths[row][col] = findPath(row,col+1,numPaths);
-            } else {
-                numPaths[row][col] = numPaths[row][col+1];
-            } //if
-            return numPaths[row][col];
-        } //if
-        
-        if (col + 1 >= colLen) {
-            if (numPaths[row+1][col] == 0) {
-                numPaths[row][col] = findPath(row+1,col,numPaths);
-            } else {
-                numPaths[row][col] = numPaths[row+1][col];
-            } //if
-            return numPaths[row][col];
-        } //if
-        
-        int pathsDown = numPaths[row+1][col];
-        int pathsRight = numPaths[row][col+1];
-        
-        if (pathsDown == 0) pathsDown = findPath(row+1,col,numPaths);
-        if (pathsRight == 0) pathsRight = findPath(row,col+1,numPaths);
-        
-        numPaths[row][col] = pathsDown + pathsRight;
-        return numPaths[row][col];
-    } //findPath
 }
