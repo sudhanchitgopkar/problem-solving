fb5d97d8b266f866ce54ab858df0d4eaeb5662a9
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-08-02 18:34:26 -0700

    Time: 1 ms (89.83%), Space: 43.8 MB (40.43%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -3,13 +3,10 @@ class Solution {
         int [] memo = new int [cost.length];
         memo[0] = cost[0];
         memo[1] = cost[1];
-        dfs(cost,memo,2);
+        
+        for (int i = 2; i < cost.length; i++) 
+            memo[i] = Math.min(memo[i - 1], memo[i - 2]) + cost[i];
+        
         return Math.min(memo[memo.length-1], memo[memo.length-2]);
     } //minCostClimbingStairs
-    
-    private void dfs (int [] cost, int [] memo, int curr) {
-        if (curr >= cost.length) return;
-        memo[curr] = Math.min(memo[curr - 1], memo[curr - 2]) + cost[curr];
-        dfs(cost,memo,curr+1);
-    } //dfs
 } //Sol
