7ed8aa9e3366b247f7ed5f1505e29dd6439e297f
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-08-09 22:57:01 -0400

    Time: 9 ms (49.91%), Space: 42.5 MB (44.81%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,20 +1,21 @@
 class Solution {
     int sol = 0;
     public int countSubstrings(String s) {
-        for (int i = 0; i < s.length(); i++)
-            dfs(s,s.substring(i,i+1),i);
+        for (int i = 0; i < s.length(); i++) {
+            expand(s,i,i);
+            if (i+1 < s.length() && s.charAt(i) == s.charAt(i+1)) {
+                expand(s,i,i+1);
+            } //if
+        } //for
+        
         return sol;
     }
     
-    private void dfs (String s, String ss, int i) {
-        if (check(ss)) sol++;
-        if (i == s.length()-1) return;
-        dfs(s,ss+s.charAt(i+1),i+1);
+    private void expand (String s, int l, int r) {
+        if (s.charAt(l) == s.charAt(r)) sol++;
+        else return;
+        if (--l < 0 || ++r >= s.length()) return;
+        expand(s,l,r);
     } //dfs
     
-    private boolean check (String s) {
-        for (int i = 0; i < s.length(); i++)
-            if (s.charAt(i) != s.charAt(s.length()-1-i)) return false;
-        return true;
-    } //isPalindrome
 }
