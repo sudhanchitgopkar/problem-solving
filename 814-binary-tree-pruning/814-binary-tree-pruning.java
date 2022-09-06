3ea275f8b66f280f10be7ca7ced4e2332476cce2
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-09-06 00:10:08 -0400

    Time: 0 ms (100.00%), Space: 42.3 MB (11.30%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -18,7 +18,6 @@ class Solution {
         if (root == null) return null;
         root.left = pruneTree(root.left);
         root.right = pruneTree(root.right);
-        if (root.left == null && root.right == null && root.val == 0) return null;
-        return root;
+        return root = root.left == null && root.right == null && root.val == 0 ? null : root;
     }
 }
