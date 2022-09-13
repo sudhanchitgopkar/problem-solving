2def4f652647dc4f17d53166a155581d11297a59
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-09-13 18:09:10 -0400

    Time: 5 ms (74.18%), Space: 43.7 MB (58.78%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -3,11 +3,10 @@ class Solution {
         if (s.length() != t.length()) return false;
         int [] freq = new int [26];
         
-        for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
-        } //for
-        
+        for (char c : s.toCharArray()) 
+            freq[c - 'a']++;
+        for (char c : t.toCharArray())
+            freq[c - 'a']--;     
         for (int f : freq) 
             if (f != 0) return false;
         
