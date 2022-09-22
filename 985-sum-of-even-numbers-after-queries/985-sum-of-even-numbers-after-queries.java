58011fa215802a7c8432ee575cf55f6e4fcaaa27
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-09-21 20:34:55 -0400

    Time: 7 ms (60.71%), Space: 75.5 MB (15.26%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,15 +1,34 @@
 class Solution {
     public int [] sumEvenAfterQueries(int[] nums, int[][] queries) {
         int [] sol = new int [queries.length];
-        int esum = 0, i = 0;
+        int solIndex = 0;
+        int esum = 0;
         
-        for (int n : nums) if (n % 2 == 0) esum += n;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                esum += num;
+            }
+        } //for
         
-        for (int [] q : queries) {
-            esum = nums[q[1]] % 2 == 0 ? esum - nums[q[1]] : esum;
-            nums[q[1]] += q[0];
-            esum = nums[q[1]] % 2 == 0 ? esum + nums[q[1]] : esum;
-            sol[i++] = esum;
+        
+        for (int [] query : queries) {
+            int val = query[0];
+            int idx = query[1];
+            
+            if (nums[idx] % 2 == 0 && (nums[idx] + val) % 2 == 0) {
+                esum += (nums[idx] + val) - nums[idx];
+            } else if (nums[idx] % 2 == 0 && (nums[idx] + val) % 2 != 0) {
+                esum -= nums[idx];
+            } else if (nums[idx] % 2 != 0 && (nums[idx] + val) % 2 == 0) {
+                esum += nums[idx] + val;
+            } else {
+                //nothing.
+            }
+            
+            nums[idx] += val;
+            
+            sol[solIndex] = esum;
+            solIndex++;
         } //for
         
         return sol;
