a480d53421a235b11bf55c5b50580ae83bfef36e
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-09-28 01:01:08 -0400

    Time: 13 ms (40.29%), Space: 62.1 MB (65.01%) - LeetHub

---
+++ 463-island-perimeter/463-island-perimeter.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int sol = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) sol += 4-countNeighbors(grid,i,j);
+            } //for
+        } //for
+        
+        return sol;
+    }
+    
+    private int countNeighbors(int[][] grid, int i, int j) {
+        int neighbors = 0;
+        if (i - 1 >= 0 && grid[i-1][j] == 1) neighbors++;
+        if (j - 1 >= 0 && grid[i][j-1] == 1) neighbors++;
+        if (i + 1 < grid.length && grid[i+1][j] == 1) neighbors++;
+        if (j + 1 < grid[0].length && grid[i][j+1] == 1) neighbors++;
+        return neighbors;
+    } //countNeighbors
+}
+
+//1->3
+//2->2
+//
