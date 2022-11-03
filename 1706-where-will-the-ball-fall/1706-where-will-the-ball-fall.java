4db4e46d892402ce2ed099b71332e0fcd7f33ce1
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-11-02 20:02:47 -0400

    Time: 167 ms (5.41%), Space: 43.6 MB (89.67%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] findBall(int[][] grid) {
+        int [] sol = new int [grid[0].length];
+        for (int i = 0; i < sol.length; i++) sol[i] = getPos(grid,0,i);
+        //System.out.println(getPos(grid,0,0));
+        return sol;
+    } //findBall
+    
+    private int getPos(int [][] grid, int row, int col) {
+        System.out.println("row: " + row + "\tcol: " + col);
+        if (row >= grid.length) return col >= 0 && col < grid[0].length ? col : -1;
+        if (col < 0 || col >= grid[0].length) return -1;
+        
+        if (grid[row][col] == 1 && col + 1 < grid[0].length && grid[row][col+1] == -1) {return -1;}
+        if (grid[row][col] == -1 && col - 1 >= 0 && grid[row][col-1] == 1) {return -1;}
+        
+        if (grid[row][col] == 1) return getPos(grid,row+1,col+1);
+        else return getPos(grid,row+1,col-1);
+    } //getPos
+} //Sol
