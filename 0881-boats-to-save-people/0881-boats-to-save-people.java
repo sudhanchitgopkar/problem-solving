2ba545163e62856af4c0e5163f73f6462ccbf2c0
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-11-15 22:46:36 -0500

    Time: 28 ms (46.37%), Space: 65.2 MB (58.39%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -4,14 +4,9 @@ class Solution {
         int l = 0, r = people.length-1, sol = 0;
         
         while (l <= r) {
-            if (people[l] + people[r] > limit) {
-                r--;
-                sol++;
-            } else {
-                l++;
-                r--;
-                sol++;
-            }
+            l = people[l] + people[r] <= limit ? l + 1 : l;
+            r--;
+            sol++;
         } //while
         
         return sol;
