62ee4762a9dbbc0a62a03db8a1b61103c0d934dc
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-11-30 18:57:39 -0500

    Time: 122 ms (84.00%), Space: 40.1 MB (96.15%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,30 @@
+class Solution {
+    boolean [][] seen;
+    public boolean exist(char[][] board, String word) {
+        seen = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) 
+            for (int j = 0; j < board[0].length; j++) 
+                if (board[i][j] == word.charAt(0) && check(board, word, i, j, 0)) 
+                    return true;
+        return false;
+    } //exist
+    
+    private boolean check (char [][] board, String word, int row, int col, int index) {
+        if (index >= word.length()) return true;
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
+        if (seen[row][col] || board[row][col] != word.charAt(index)) return false;
+        
+        seen[row][col] = true;
+        
+        if (
+            check(board,word,row-1,col,index+1) ||
+            check(board,word,row+1,col,index+1) ||
+            check(board,word,row,col-1,index+1) ||
+            check(board,word,row,col+1,index+1)
+           ) 
+            return true;
+        
+        seen[row][col] = false;
+        return false;
+    } //check
+}
