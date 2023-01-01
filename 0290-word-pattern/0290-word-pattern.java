543c1b977c3819de6eedbc574ceb169cd3aa7f41
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2022-12-31 19:18:22 -0500

    Time: 1 ms (95.36%), Space: 40.4 MB (70.29%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         HashMap <Character, String> lookup = new HashMap <> ();
-        HashSet <String> seen = new HashSet <> ();
         String [] words = s.split(" ");
         char [] keys = pattern.toCharArray();
 
@@ -9,10 +8,9 @@ class Solution {
 
         for (int i = 0; i < keys.length; i++) {
             if (!lookup.containsKey(keys[i])) 
-                if (seen.contains(words[i])) return false;
+                if (lookup.containsValue(words[i])) return false;
                 else lookup.put(keys[i], words[i]);
             else if (!lookup.get(keys[i]).equals(words[i])) return false;
-            seen.add(words[i]);
         } //for
 
         return true;
