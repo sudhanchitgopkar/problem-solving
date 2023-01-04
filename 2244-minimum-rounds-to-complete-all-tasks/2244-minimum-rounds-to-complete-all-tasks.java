bdd554bb416feaa5c04b91588382e9ee6d2448fc
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-01-03 23:35:52 -0500

    Time: 90 ms (38.96%), Space: 105.3 MB (32.70%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -4,10 +4,13 @@ class Solution {
         int sol = 0;
         for (int task : tasks) diff.put(task, diff.getOrDefault(task, 0) + 1);
         
-        for (int d : diff.keySet()) 
-            if (diff.get(d) == 1) return -1;
-            else sol += diff.get(d) % 3 == 0 ? diff.get(d)/3 : diff.get(d)/3 + 1;
-        
+        for (int d : diff.keySet()) {
+            int val = diff.get(d);
+            if (val == 1) return -1;
+            sol += val/3;
+            if (val % 3 != 0) sol++;
+        } //for
+             
 
          return sol;
     } //minRounds
