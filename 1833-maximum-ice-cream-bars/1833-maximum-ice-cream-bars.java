41dcbb267715eab5257d0ada62cffd6b1e2b140e
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-01-06 11:04:56 -0500

    Time: 7 ms (98.58%), Space: 80.9 MB (5.92%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,14 +1,23 @@
-class Solution:
-    def maxIceCream(self, costs: List[int], coins: int) -> int:
-        costs.sort()
-        sol = 0
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int max = Integer.MIN_VALUE, sol = 0, bucketidx = 0;
+        for (int cost : costs) max = cost > max ? cost : max;
+        int [] buckets = new int [max + 1];
+        for (int cost : costs) buckets[cost]++;
         
-        for cost in costs:
-            coins -= cost
-            if coins < 0:
-                return sol
-            else:
-                sol += 1
+        while (coins > 0 && bucketidx < buckets.length) {
+            if (buckets[bucketidx] == 0) {
+                bucketidx++;
+            } else if (coins >= bucketidx * buckets[bucketidx]) {
+                coins -= bucketidx * buckets[bucketidx];
+                sol += buckets[bucketidx++];
+            } else if (coins >= bucketidx) {
+                coins -= bucketidx;
+                buckets[bucketidx]--;
+                sol++;
+            } else break;
+        } //while
         
-        return sol
-        
+        return sol;
+    } //maxIceCream
+} //Sol
