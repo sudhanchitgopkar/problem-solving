d0993926351803f1e606e8148c03c79a12ab1844
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-01-08 16:17:18 -0500

    Time: 1966 ms (5.03%), Space: 56 MB (21.34%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,29 +1,25 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        HashMap <String, HashSet<String>> lines = new HashMap <> ();
+        HashMap <String, Integer> lines = new HashMap <> ();
         int max = 1;
 
         for (int i = 0; i < points.length; i++) {
             int [] curr = points[i];
-            for (int j = i + 1; j < points.length; j++) {
+            for (int j = 0; j < points.length; j++) {
                 int [] ref = points[j];
+                if (i == j) continue;
                 if (curr[0] == ref[0]) { //points are vertical
-                    if (!lines.containsKey("V" + curr[0])) 
-                        lines.put("V" + curr[0], new HashSet <String> ());
-                    lines.get("V" + curr[0]).add("(" + curr[0] + "," + curr[1] + ")");
-                    lines.get("V" + curr[0]).add("(" + ref[0] + "," + ref[1] + ")");
+                    lines.put(i + "V" + curr[0], lines.getOrDefault(i + "V" + curr[0],1) + 1);
                 } else { 
                     double m = ((ref[1] - curr[1] * 1.0)/(ref[0] - curr[0]));
                     double b = ref[1] - (m * ref[0]);
-                    if (!lines.containsKey("M" + m + "B" + b))
-                        lines.put("M" + m + "B" + b, new HashSet <String> ());
-                    lines.get("M" + m + "B" + b).add("(" + curr[0] + "," + curr[1] + ")");
-                    lines.get("M" + m + "B" + b).add("(" + ref[0] + "," + ref[1] + ")");
+                    String sm = String.format("%.8f", m), sb = String.format("%.8f", b);
+                    lines.put(i + "M" + sm + "B" + sb, lines.getOrDefault(i + "M" + sm + "B" + sb, 1) + 1);
                 } //else
             } //for
         } //for
 
-        for (String line : lines.keySet()) max = Math.max(max, lines.get(line).size());
+        for (String line : lines.keySet()) max = Math.max(max, lines.get(line));
         return max;
     } //maxPoints
 } //Sol
