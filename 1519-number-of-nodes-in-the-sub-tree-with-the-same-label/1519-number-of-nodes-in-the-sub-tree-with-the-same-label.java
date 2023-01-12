f5f792d3c8fe74f6d9af6fd3fcdc262da8201052
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-01-11 23:45:38 -0500

    Time: 77 ms (92.39%), Space: 137.6 MB (67.39%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
@@ -0,0 +1,36 @@
+class Solution {
+    int [] sol;
+    char [] id;
+    List<List<Integer>> graph;
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        sol = new int [n];
+        int [] freq = new int [26];
+        id = labels.toCharArray();
+        graph = new ArrayList<List<Integer>> ();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<Integer> ());
+        } //for
+
+        for (int [] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        } //for
+
+        dfs(0,0,freq);
+        return sol;
+    } //countSub
+
+    private int [] dfs (int curr, int parent, int [] freq) {
+        int [] newFreq = Arrays.copyOf(freq,freq.length);
+        for (int next : graph.get(curr)) {
+            if (next == parent) continue;
+            int [] res = dfs(next,curr,freq);
+            for (int i = 0; i < res.length; i++) newFreq[i] += res[i] - freq[i];
+        } //for
+
+        newFreq[id[curr]-'a']++;
+        sol[curr] = newFreq[id[curr]-'a'];
+        return newFreq;
+    } //dfs
+} //Sol
