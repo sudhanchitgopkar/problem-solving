1a70d6ef66cff15b2bae4236ab0d9871cf2516b7
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-05-15 10:26:22 -0400

    Time: 60 ms (41.15%), Space: 16.8 MB (19.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,33 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        sfreq = {}
+        tfreq = {}
+        
+        for char in s:
+            if char in sfreq:
+                sfreq[char] = sfreq[char] + 1
+            else:
+                sfreq[char] = 1
+                
+        for char in t:
+            if char in tfreq:
+                tfreq[char] = tfreq[char] + 1
+            else:
+                tfreq[char] = 1
+        
+        for char in sfreq:
+            if char in tfreq:
+                if sfreq[char] != tfreq[char]:
+                    return False
+            else:
+                return False
+        
+        for char in tfreq:
+            if char in sfreq:
+                if sfreq[char] != tfreq[char]:
+                    return False
+            else:
+                return False
+        
+        return True
+        
