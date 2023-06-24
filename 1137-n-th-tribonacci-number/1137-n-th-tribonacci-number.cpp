9e7cff26583544497d1f3ad4c0cb871dbe6f4e78
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-06-24 17:22:18 -0400

    Time: 0 ms (100.00%), Space: 6.1 MB (20.65%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector <int> dp {0, 1, 1};
+        
+        for (int i = 3; i <= n; i++) {
+            dp.push_back(dp[i - 1] + dp[i - 2] + dp[i - 3]);
+        } //for
+        
+        return dp[n];
+    } //trib
+};
