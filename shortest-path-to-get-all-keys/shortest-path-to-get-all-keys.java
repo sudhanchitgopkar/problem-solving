b1dbff93489621d5482d78673c6c7e390d85e8cf
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-06-30 13:31:47 -0400

    Time: 114 ms (30.49%) | Memory: 52.2 MB (29.88%) - LeetSync

--- shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.java
+++ shortest-path-to-get-all-keys/shortest-path-to-get-all-keys.java
@@ -1,82 +1,64 @@
 class Solution {
+    class State {
+        int keys, i, j;
+        State(int keys, int i, int j) {
+            this.keys = keys;
+            this.i = i;
+            this.j = j;
+        }
+    }
     public int shortestPathAllKeys(String[] grid) {
-        int m = grid.length, n = grid[0].length();
-        Queue<int[]> queue = new LinkedList<>();
-        int[][] moves = new int[][] {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        
-        // seen['key'] is only for BFS with key state equals 'keys'
-        Map<Integer, Set<Pair<Integer, Integer>>> seen = new HashMap<>();
-        
-        Set<Character> keySet = new HashSet<>();
-        Set<Character> lockSet = new HashSet<>();
-        int allKeys = 0;
-        int startR = -1, startC = -1;
+        int x = -1, y = -1, m = grid.length, n = grid[0].length(), max = -1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                char cell = grid[i].charAt(j);
-                if (cell >= 'a' && cell <= 'f') {
-                    allKeys += (1 << (cell - 'a'));
-                    keySet.add(cell);
+                char c = grid[i].charAt(j);
+                if (c == '@') {
+                    x = i;
+                    y = j;
                 }
-                if (cell >= 'A' && cell <= 'F') {
-                    lockSet.add(cell);
-                }
-                if (cell == '@') {
-                    startR = i;
-                    startC = j;
+                if (c >= 'a' && c <= 'f') {
+                    max = Math.max(c - 'a' + 1, max);
                 }
             }
         }
-
-        // [row, column, key state, distance]
-        queue.offer(new int[] {startR, startC, 0, 0});
-        seen.put(0, new HashSet<>());
-        seen.get(0).add(new Pair<>(startR, startC));    
-        
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int curR = cur[0], curC = cur[1], keys = cur[2], dist = cur[3];
-            for (int[] move : moves) {
-                int newR = curR + move[0], newC = curC + move[1];
-                
-                // If this cell (newR, newC) is reachable.
-                if (newR >= 0 && newR < m && newC >= 0 && newC < n && grid[newR].charAt(newC) != '#') {
-                    char cell = grid[newR].charAt(newC);
-                    
-                    // If it is a key:
-                    if (keySet.contains(cell)) {
-                        // If we have collected it before, no need to revisit this cell.
-                        if (((1 << (cell - 'a')) & keys) != 0) {
+        State start = new State(0, x, y);
+        Queue<State> q = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        visited.add(0 + " " + x + " " + y);
+        q.offer(start);
+        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int step = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                State cur = q.poll();
+                if (cur.keys == (1 << max) - 1) {
+                    return step;
+                }
+                for (int[] dir : dirs) {
+                    int i = cur.i + dir[0];
+                    int j = cur.j + dir[1];
+                    int keys = cur.keys;
+                    if (i >= 0 && i < m && j >= 0 && j < n) {
+                        char c = grid[i].charAt(j);
+                        if (c == '#') {
                             continue;
                         }
-                        
-                        // Otherwise, we can walk to this cell and pick it up.
-                        int newKeys = (keys | (1 << (cell - 'a')));
-                        
-                        // If we collect all keys, return dist + 1. 
-                        // Otherwise, just add this state to seen and queue.
-                        if (newKeys == allKeys) {
-                            return dist + 1;
+                        if (c >= 'a' && c <= 'f') {
+                            keys |= 1 << (c - 'a');
                         }
-                        seen.putIfAbsent(newKeys, new HashSet<>());
-                        seen.get(newKeys).add(new Pair<>(newR, newC));
-                        queue.offer(new int[] {newR, newC, newKeys, dist + 1});
-                    }
-                    
-                    // If it is a lock and we don't have its key, continue.
-                    if (lockSet.contains(cell) && ((keys & (1 << (cell - 'A'))) == 0)) {
-                        continue;
-                    }
-                        
-                    // We can walk to this cell if we haven't been here before with the same key state.
-                    else if (!seen.get(keys).contains(new Pair<>(newR, newC))) {
-                        seen.get(keys).add(new Pair<>(newR, newC));
-                        queue.offer(new int[] {newR, newC, keys, dist + 1});
+                        if (c >= 'A' && c <= 'F' && ((keys >> (c - 'A')) & 1) == 0) {
+                            continue;
+                        }
+                        if (!visited.contains(keys + " " + i + " " + j)) {
+                            visited.add(keys + " " + i + " " + j);
+                            q.offer(new State(keys, i, j));
+                        }
                     }
                 }
             }
+            step++;
         }
-        
         return -1;
     }
 }
