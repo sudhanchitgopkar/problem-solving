61015301313887114a732bdd57556d2c92909cc8
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-06-30 21:49:32 -0400

    Time: 2 ms (95.40%) | Memory: 6.9 MB (23.17%) - LeetSync

--- fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
+++ fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -15,9 +15,9 @@ public:
         int sol = INT_MAX;
         for (int kid = 0; kid < k; kid++) {
             kids[kid] += cookies[i];
-            if (sol == cookies.size()/k) return sol;
             if (kids[kid] < sol) sol = min(sol, distribute(i + 1, k, cookies, kids));
             kids[kid] -= cookies[i];
+            if (kids[kid] == 0) break;
         } //for 
 
         return sol;
