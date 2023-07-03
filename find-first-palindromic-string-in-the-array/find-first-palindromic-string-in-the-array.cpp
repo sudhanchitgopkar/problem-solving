c7d7cb3f8d116cdf286c2dd7756adf5e1c807e06
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-07-03 15:21:29 -0400

    Time: 70 ms (26.63%) | Memory: 22.9 MB (63.11%) - LeetSync

--- find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
         for (string word : words) {
-            if (isPal(word)) return word;
+            string_view sv (word);
+            if (isPal(sv)) return static_cast<string>(sv);
         } //for
         return "";
     } //firstPalindrome
 
-    bool isPal(string word) {
+    bool isPal(string_view word) {
         int n = word.size(); 
         for (int i = 0; i < n/2; i++) {
             if (word.at(i) != word.at(n - 1 - i)) return false;
