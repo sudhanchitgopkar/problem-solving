5458533b498dd78b8b0735c7c5aa32cedab738a5
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-07-12 21:34:08 -0400

    Time: 23 ms (71.93%) | Memory: 14.2 MB (34.65%) - LeetSync

--- course-schedule/course-schedule.cpp
+++ course-schedule/course-schedule.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prereqs) {
-        vector<vector<int>> adj (numCourses);
-        vector<int> seen (numCourses, -1);
+    bool canFinish(int n, vector<vector<int>>& prereqs) {
+        vector<int> sorted (n), in (n);
+        vector<vector<int>> adj (n);
+        queue <int> q;
 
+        //populate adj and in list
         for (vector<int> prereq : prereqs) {
             adj[prereq[1]].push_back(prereq[0]);
+            in[prereq[0]]++;
         } //for
 
-        for (int i = 0; i < numCourses; i++) {
-            if (seen[i] == -1 && hasCycle(i, adj, seen)) {
+        for (int i = 0; i < n; i++) {
+            if (in[i] == 0) {
+                q.push(i);
+            } //if
+        } //for
+
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for (int next : adj[curr]) {
+                if (--in[next] == 0) {
+                    q.push(next);
+                } //if
+            } //if
+            sorted.push_back(curr);
+        } //while
+
+        for (int i = 0; i < n; i++) {
+            if (in[i] != 0) {
                 return false;
             } //if
         } //for
-
+        
         return true;
     } //canFinish
-
-    bool hasCycle (int curr, vector<vector<int>>& adj, vector<int>& seen) {
-        if (seen[curr] == 1) {
-            return true;
-        } else if (seen[curr] == 0) {
-            return false;
-        } //if
-
-        seen[curr] = 1;
-
-        for (int next : adj[curr]) {
-            if (hasCycle(next, adj, seen)) {
-                return true;
-            } //if
-        } //for
-
-        seen[curr] = 0;
-        return false;
-    } //cycle
 };
