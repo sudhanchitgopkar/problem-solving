57b445058b0438359dd8d0096ec951a4f6ff2b1a
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-07-13 21:25:10 -0400

    Time: 166 ms (53.11%) | Memory: 54.8 MB (68.37%) - LeetSync

--- longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
+++ longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& arr, int diff) {
-        int n = arr.size(), sol = 1;
-        unordered_map<int, int> dp;
+        unordered_map <int, int> subseqs;
+        int sol = 1;
 
-        for (int i = n - 1; i >= 0; i--) {
-            dp[arr[i] - diff] =  dp[arr[i]] + 1;
-        } //for
-
-        for (auto & entry : dp) {
-            sol = max(sol, entry.second);
+        for (int val : arr) {
+            if (subseqs.count(val) > 0) {
+                subseqs[val + diff] = subseqs[val] + 1;
+                sol = max(sol, subseqs[val + diff]);
+            } else {
+                subseqs[val + diff] = 1;
+            } //if
         } //for
 
         return sol;
