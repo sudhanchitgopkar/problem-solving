345881cd492b07a47a7f94064e7f9ea1eecfe039
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-07-17 22:27:02 -0400

    Time: 618 ms (5.01%) | Memory: 124.6 MB (23.21%) - LeetSync

---
+++ LeetCode/146-lru-cache/lru-cache.java
@@ -0,0 +1,69 @@
+class LRUCache {
+    HashMap <Integer, Node> hm = new HashMap <> ();
+    Node LRU = null, MRU = null;
+    int size = 0, cap = 0;
+
+    public LRUCache(int capacity) {
+        cap = capacity;
+    } //constructor
+    
+    public int get(int key) {
+        if (hm.containsKey(key)) {
+            Node curr = hm.get(key);
+
+            if (curr.val == -1) {
+                return -1;
+            } else if (LRU == curr && size > 1) {
+                LRU = LRU.next;
+            } else if (curr == MRU) {
+                return MRU.val;
+            } //if
+
+            if (curr.next != null) curr.next.prev = curr.prev;
+            if (curr.prev != null) curr.prev.next = curr.next;
+            curr.next = null;
+            curr.prev = MRU;
+            MRU.next = curr;
+            MRU = curr;
+            return MRU.val;
+        } else {
+            return -1;
+        } //if
+    } //get
+    
+    public void put(int key, int value) {
+        if (get(key) == -1) {
+            Node newNode = new Node (null, MRU, value);
+            hm.put(key, newNode);
+            if (LRU == null) LRU = newNode;
+            if (MRU == null) {
+                MRU = newNode;   
+            } else {
+                MRU.next = newNode;
+                MRU = MRU.next;
+            } //if
+
+            if (++size > cap) {
+                LRU.val = -1;
+                LRU = LRU.next;
+                --size;
+            } //if
+        } else {
+            MRU.val = value;
+        } //if
+        System.out.println("PUT: " + key + " -> " + value);
+        System.out.println("MRU IS: " + MRU.val);
+    } //put
+} //LRUCache
+
+class Node {
+    protected Node next, prev;
+    int val;
+
+    public Node(Node next, Node prev, int val) {
+        this.next = next;
+        this.prev = prev;
+        this.val = val;
+    } //Node
+} //Node
+
