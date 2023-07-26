1b608f35eabeca47352402fb6f2a48135394215a
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-07-25 21:49:55 -0400

    Time: 374 ms (39.71%) | Memory: 101.5 MB (26.33%) - LeetSync

---
+++ LeetCode/2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minSpeedOnTime(vector<int>& dists, double deadline) {
+        if (dists.size() - 1 >= deadline) return -1;
+        int l = 0, n = dists.size(), r = 10e7;
+
+        while (l <= r) {
+            int m = l + (r - l)/2;
+            bool can = check(dists, deadline, m);
+            if (can) {
+                r = --m;
+            } else {
+                l = ++m;
+            } //if
+        } //while   
+
+        return l == 0 ? 1 : l;
+    } //minSpeedOnTime
+
+    bool check (vector<int>& dists, double deadline, int speed) {
+        double currTime = 0.0;
+
+        for (int i = 0; i < dists.size(); i++) {
+            currTime += ((dists[i] * 1.0)/speed);
+            if (i != dists.size() - 1) currTime = ceil(currTime);
+        } //for
+
+        return currTime <= deadline;
+    } //can
+};
