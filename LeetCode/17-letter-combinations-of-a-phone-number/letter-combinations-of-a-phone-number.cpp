f060105745bffa425338abcc6560b4d377deeb76
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-02 21:11:53 -0400

    Time: 0 ms (100.00%) | Memory: 6.8 MB (13.39%) - LeetSync

---
+++ LeetCode/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> sol;
+    vector<string> letterCombinations(string digits) {
+        if (digits.size() == 0) return {};
+        unordered_map<char, vector<char>> um;
+
+        um['2'] = {'a', 'b', 'c'};
+        um['3'] = {'d', 'e', 'f'};
+        um['4'] = {'g', 'h', 'i'};
+        um['5'] = {'j', 'k', 'l'};
+        um['6'] = {'m', 'n', 'o'};
+        um['7'] = {'p', 'r', 'q', 's'};
+        um['8'] = {'t', 'u', 'v'};
+        um['9'] = {'w', 'x', 'y', 'z'};
+
+        dfs(0, "", um, digits);
+        return sol;
+    } //letterCombos
+
+    void dfs(int i, string curr, unordered_map<char, vector<char>>& um, string digits) {
+        if (i == digits.size()) {
+            sol.push_back(curr);
+        } //if
+
+        for (char c : um[digits[i]]) {
+            string next = curr + c;
+            dfs(i + 1, next, um, digits);
+        } //for
+    } //dfs
+};
