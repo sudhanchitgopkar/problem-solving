c77e25ae42bfa7079be0efa6888a61c502eaf132
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-03 22:28:26 -0400

    Time: 22 ms (34.52%) | Memory: 14.5 MB (26.06%) - LeetSync

--- LeetCode/139-word-break/word-break.cpp
+++ LeetCode/139-word-break/word-break.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
+    unordered_set<string> dict;
     bool wordBreak(string s, vector<string>& wordDict) {
         vector<int> dp (s.size(), -1);
-        return canBreak(0, s, wordDict, dp);
+
+        for (string s : wordDict) {
+            dict.insert(s);
+        } //for
+
+        return canBreak(0, s, dict, dp);
     } //break
 
-    bool canBreak(int i, string s, vector<string>& dict, vector<int>& dp) {
+    bool canBreak(int i, string s, unordered_set<string>& dict, vector<int>& dp) {
         if (i >= s.size()) {
             return true;
         } else if (dp[i] != -1) {
             return dp[i];
         } //if
-        
-        for (string word : dict) {
-            if (word[0] == s[i] && word.size() <= s.size() - i) {
-                bool valid = true;
-                //cout << "TRYING: " << word << "\n";
-                for (int j = 0; j < word.size(); j++) {
-                    if (word[j] != s[i + j]) {
-                        valid = false;
-                        break;
-                    } //if
-                } //for
-                if (valid) {
-                    if (canBreak(i + word.size(), s, dict, dp)) {
-                        dp[i] = 1;
-                        return true;
-                    } //if
+
+        int j = i;
+        while (j < s.size()) {
+            if (dict.count(s.substr(i, j - i + 1))) {
+                if (canBreak(j + 1, s, dict, dp)) {
+                    return true;
                 } //if
             } //if
-        } //for 
+            j++;
+        } //while
 
         dp[i] = 0;
         return dp[i];
