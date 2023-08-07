2d6b42c82c350a6186d6ee07672324bfd4f345a0
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-06 21:15:51 -0400

    Time: 0 ms (100.00%) | Memory: 9.6 MB (19.73%) - LeetSync

--- LeetCode/74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ LeetCode/74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -2,13 +2,37 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
         int m = mat.size(), n = mat[0].size();
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == target) return true;
+        int row = searchRow(0, m - 1, target, mat);
+        if (row == -1) return false;
+        
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int m = l + (r - l)/2;
+            if (mat[row][m] == target) {
+                return true;
+            } else if (mat[row][m] < target) {
+                l = ++m;
+            } else {
+                r = --m;
             } //if
-        } //for
+        } //while
 
         return false;
-    } //searchMat
+    } //search
+
+    int searchRow(int l, int r, int target, vector<vector<int>>& mat) {
+        while (l <= r) {
+            int m = l + (r - l)/2;
+
+            if (mat[m][0] <= target && mat[m][mat[0].size() - 1] >= target) {
+                return m;
+            } else if (mat[m][0] > target) {
+                r = --m;
+            } else {
+                l = ++m;
+            } //if
+        } //while
+
+        return -1;
+    } //search
 };
