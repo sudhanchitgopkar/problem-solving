be5f60d032c20c29621d7be33700209e474438e1
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-07 21:22:46 -0400

    Time: 0 ms (100.00%) | Memory: 40.6 MB (92.66%) - LeetSync

--- LeetCode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ LeetCode/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -8,11 +8,15 @@ class Solution {
     
             if (nums[m] == target)
                 return m;
-            else if (nums[l] <= nums[m] && nums[l] <= target && nums[m] >= target)
+            else if (nums[l] <= nums[m] && 
+                     nums[l] <= target && 
+                     nums[m] >= target)
                 r = m-1;
             else if (nums[l] <= nums[m])
                 l = m+1;
-            else if (nums[m] <= nums[r] && nums[m] <= target && nums[r] >= target)
+            else if (nums[m] <= nums[r] && 
+                     nums[m] <= target && 
+                     nums[r] >= target)
                 l = m+1;
             else
                 r = m-1;
