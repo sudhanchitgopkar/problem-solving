660e53e6a02c06ff7d4006236d2312289a16fae8
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-09 21:34:43 -0400

    Time: 7 ms (53.92%) | Memory: 14.1 MB (18.44%) - LeetSync

--- LeetCode/81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ LeetCode/81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,35 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for (int num : nums) {
-            if (num == target) return true;
-        } //for
+        int n = nums.size(), l = 0, r = n - 1;
+        if (n == 1) return target == nums[0];
+
+        while (l <= r) {
+            if (nums[l] == target || nums[r] == target) return true;
+            while (l < n && nums[l] == nums[r]) ++l;
+            if (l > r) return false;
+
+            int m = l + (r - l)/2;
+            if (nums[m] == target) {
+                return true;
+            } //if
+            
+            if (nums[m] == target)
+                return m;
+            else if (nums[l] <= nums[m] && 
+                     nums[l] <= target && 
+                     nums[m] >= target)
+                r = m-1;
+            else if (nums[l] <= nums[m])
+                l = m+1;
+            else if (nums[m] <= nums[r] && 
+                     nums[m] <= target && 
+                     nums[r] >= target)
+                l = m+1;
+            else
+                r = m-1;
+        } //while
+
         return false;
     } //search
 };
