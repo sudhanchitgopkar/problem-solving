80324c74883164f1bb74259aeea1c10f0f638d53
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-15 21:53:28 -0400

    Time: 78 ms (18.69%) | Memory: 62 MB (26.02%) - LeetSync

--- LeetCode/239-sliding-window-maximum/sliding-window-maximum.java
+++ LeetCode/239-sliding-window-maximum/sliding-window-maximum.java
@@ -2,27 +2,28 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int soli = 0, n = nums.length, l = 0, r = 0;
         int [] sol = new int [n - k + 1];
-        PriorityQueue <Pair> pq = new PriorityQueue <> (new Comparator<Pair>() {
+        //int [] -> val, index
+        PriorityQueue <int []> pq = new PriorityQueue <> (new Comparator<int []>() {
             @Override
-            public int compare(Pair a, Pair b) {
-                return Integer.compare(b.val, a.val);
+            public int compare(int [] a, int [] b) {
+                return Integer.compare(b[0], a[0]);
             } //compare
         });
 
 
         //populate pq with initial window
         for (; r < k; r++) {
-            pq.offer(new Pair(nums[r], r));
+            pq.offer(new int [] {nums[r], r});
         } //for
 
-        sol[soli++] = pq.peek().val;
+        sol[soli++] = pq.peek()[0];
 
         while (r < n) {
-            pq.offer(new Pair(nums[r], r++));
+            pq.offer(new int [] {nums[r], r++});
             ++l;
 
-            while (pq.peek().index < l) pq.poll();
-            sol[soli++] = pq.peek().val;
+            while (pq.peek()[1] < l) pq.poll();
+            sol[soli++] = pq.peek()[0];
         } //while
 
         return sol;
