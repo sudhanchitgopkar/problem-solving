af94242b7c630d283dde18e8012b3aabae961065
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-16 21:58:58 -0400

    Time: 215 ms (8.15%) | Memory: 72.4 MB (5.01%) - LeetSync

--- LeetCode/542-01-matrix/01-matrix.cpp
+++ LeetCode/542-01-matrix/01-matrix.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size(), dist = 0;
-        vector<vector<int>> sol (m, vector<int> (n, INT_MAX));
+        vector<vector<int>> sol (m, vector<int> (n));
+        vector<vector<bool>> seen (m, vector<bool> (n));
+
         vector<vector<int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         queue <pair<int, int>> q;
 
@@ -10,20 +12,24 @@ public:
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
                     q.push(pair(i, j));
+                    seen[i][j] = true;
                 } //if
             } //for
         } //for
+
         while (!q.empty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
                 pair <int, int> curr = q.front();
                 q.pop();
-                if (sol[curr.first][curr.second] < dist) continue;
+                
                 sol[curr.first][curr.second] = dist;
+
                 for (vector<int> dir : dirs) {
                     int nextX = curr.first + dir[0], nextY = curr.second + dir[1];
                     if (nextX < 0 || nextX >= m || nextY < 0 || nextY >= n) continue;
-                    if (sol[nextX][nextY] <= dist) continue;
+                    if (seen[nextX][nextY]) continue;
+                    seen[nextX][nextY] = true;
                     q.push(pair(nextX, nextY));
                 } //for
             } //for
