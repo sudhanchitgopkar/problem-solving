a02e9e3d2bb8d8be1c452e4c2e6e2f06bdbf02cc
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-17 21:46:16 -0400

    Time: 86 ms (66.29%) | Memory: 39 MB (33.56%) - LeetSync

--- LeetCode/1738-maximal-network-rank/maximal-network-rank.cpp
+++ LeetCode/1738-maximal-network-rank/maximal-network-rank.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<unordered_set<int>> adj;
+        vector<unordered_set<int>> adj (n, unordered_set<int> ());
         int sol = 0;
-
-        for (int i = 0; i < n; i++) {
-            adj.push_back(unordered_set<int> ());
-        } //for
 
         for (vector<int> & road : roads) {
             adj[road[0]].insert(road[1]);
