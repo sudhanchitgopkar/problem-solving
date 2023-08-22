0766039fd80df613d4de36983b384aed1ae82d46
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-21 21:35:11 -0400

    Time: 6 ms (34.08%) | Memory: 40.1 MB (35.07%) - LeetSync

--- LeetCode/168-excel-sheet-column-title/excel-sheet-column-title.java
+++ LeetCode/168-excel-sheet-column-title/excel-sheet-column-title.java
@@ -2,11 +2,10 @@ class Solution {
     public String convertToTitle(int num) {
         String sol = "";
         
-        while (num > 0) {
+        while (--num >= 0) {
             char c = (char)((num % 26) + ('A'));
-            if (--c == '@') c = 'Z';
+            //if (c == '@') c = 'Z';
             sol = c + sol;
-            --num;
             num /= 26;
         } //while
 
