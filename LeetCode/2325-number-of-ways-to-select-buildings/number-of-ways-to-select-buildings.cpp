21070fc8c3c4517d566a0344ba2c15f8afe46664
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-23 18:24:32 -0400

    Time: 115 ms (71.21%) | Memory: 24.3 MB (76.63%) - LeetSync

--- LeetCode/2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.cpp
+++ LeetCode/2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
     long long numberOfWays(string s) {
-        unordered_map<string, long long> freq;
-        freq.insert({"0", 0});
-        freq.insert({"01", 0});
-        freq.insert({"010", 0});
-        freq.insert({"1", 0});
-        freq.insert({"10", 0});
-        freq.insert({"101", 0});
-        
+        vector<long long> freq (6, 0);
+        //0, 01, 010, 1, 10, 101
         for (char c : s) {
             if (c == '0') {
-                freq["0"]++;
-                freq["010"] += freq["01"];
-                freq["10"] += freq["1"];
+                freq[0]++;
+                freq[2] += freq[1];
+                freq[4] += freq[3];
             } else {
-                freq["1"]++;
-                freq["01"] += freq["0"];
-                freq["101"] += freq["10"];
+                freq[3]++;
+                freq[1] += freq[0];
+                freq[5] += freq[4];
             } //if
         } //for
 
 
-        return freq["010"] + freq["101"];
+        return freq[2] + freq[5];
     } //numWays
 };
-
-/*
-1,1,1,0,0
-
- 0   01  010
-[2] [0]  [0]
-
- 1   10  101
-[3] [6]  [0]
-
-num0s 3 2 2 2 1 0
-num1s 3 3 3 2 2 1
-
-1 -> 0
-2 -> 0
-3 -> 
-    - 010
-    - 101
-*/
