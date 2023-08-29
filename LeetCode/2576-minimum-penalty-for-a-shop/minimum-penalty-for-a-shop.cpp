ee1f16cdd174129d3175bfc1dc62e1d981effe2a
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-28 23:19:28 -0400

    Time: 36 ms (40.54%) | Memory: 18.9 MB (26.64%) - LeetSync

---
+++ LeetCode/2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        int n = customers.size(), sol = -1, minPenalty = INT_MAX;
+        vector<int> pre (n), suf(n);
+        
+        pre[0] = customers[0] == 'N' ? 1 : 0;
+        suf[n - 1] = customers[n - 1] == 'Y' ? 1 : 0;
+
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] + (customers[i] == 'N' ? 1 : 0);
+        } //for
+
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = suf[i + 1] + (customers[i] == 'Y' ? 1 : 0);
+        } //for
+        
+        for (int i = 0; i <= n; i++) {
+            int penalty = 0;
+            if (i == 0) {
+                penalty = suf[i];
+            } else if (i == n) {
+                penalty = pre[i - 1];
+            } else {
+                penalty = pre[i - 1] + suf[i];
+            } //if
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                sol = i;
+            } //if
+        } //for
+        
+        return sol;
+    } //bestClose
+};
