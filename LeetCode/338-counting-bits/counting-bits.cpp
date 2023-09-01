814c74be6ac61e5ee2093e54066221fb2a8596d4
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-08-31 23:29:39 -0400

    Time: 7 ms (40.85%) | Memory: 8.5 MB (32.26%) - LeetSync

--- LeetCode/338-counting-bits/counting-bits.cpp
+++ LeetCode/338-counting-bits/counting-bits.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> sol;
-        sol.push_back(0);
+        vector<int> sol {0};
         for (int i = 1; i <= n; i++)
             sol.push_back(sol[i/2] + i % 2);
         return sol;
