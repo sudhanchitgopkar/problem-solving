acf82ffd77f48264b752a94dbe054f5655ea0fb0
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-09-02 23:02:08 -0400

    Time: 0 ms (100.00%) | Memory: 6.6 MB (24.88%) - LeetSync

--- LeetCode/62-unique-paths/unique-paths.cpp
+++ LeetCode/62-unique-paths/unique-paths.cpp
@@ -7,9 +7,8 @@ public:
 
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (dp[i][j] == 0) {
-                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-                } //if
+                if (dp[i][j] != 0) continue;
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
             } //for
         } //for
 
