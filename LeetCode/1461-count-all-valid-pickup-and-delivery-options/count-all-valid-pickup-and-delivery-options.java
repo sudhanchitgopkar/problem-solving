2ef3f3fdf848f988af7e88cfb03760924802dac8
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-09-09 22:09:43 -0400

    Time: 1 ms (39.63%) | Memory: 39.8 MB (38.41%) - LeetSync

--- LeetCode/1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ LeetCode/1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -1,14 +1,13 @@
 class Solution {
     public int countOrders(int n) {
-        long [] dp = new long [n + 1];
-        dp[1] = 1;
+        long prev = 1;
 
         for (int i = 2; i <= n; i++) {
             int val = 2 * (i - 1) + 1;
-            long res =  dp[i - 1] * ((val * ++val)/2);
-            dp[i] = (long) (res % (Math.pow(10, 9) + 7));
+            long res =  prev * ((val * ++val)/2);
+            prev = (long) (res % (Math.pow(10, 9) + 7));
         } //for
 
-        return (int) (dp[n] % (Math.pow(10, 9) + 7));
+        return (int) (prev % (Math.pow(10, 9) + 7));
     } //countOrders
 } //Sol
