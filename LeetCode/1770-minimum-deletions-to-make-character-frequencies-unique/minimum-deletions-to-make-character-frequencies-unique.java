0a0290a0b2d2c4192e8202f428cac528c5bc2550
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2023-09-11 21:43:12 -0400

    Time: 8 ms (82.34%) | Memory: 44.6 MB (49.84%) - LeetSync

--- LeetCode/1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ LeetCode/1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,35 +1,23 @@
 class Solution {
     public int minDeletions(String s) {
        int [] freq = new int [26];
-       int [] buckets = new int [s.length() + 1];
+       HashSet <Integer> seen = new HashSet <> ();
        int sol = 0;
 
        for (char c : s.toCharArray()) {
            freq[c - 'a']++;
        } //for
 
-       for (int i = 0; i < 26; i++) {
-           if (freq[i] > 0) {
-               buckets[freq[i]]++;
-           } //if
-       } //for
-
-       for (int i = 1; i < buckets.length; i++) {
-           while (buckets[i] > 1) {
-               int closestEmpty = searchEmpty(i, buckets);
-               buckets[closestEmpty] = closestEmpty == 0 ? 0 : 1;
-               buckets[i]--;
-               sol += i - closestEmpty;
+       for (int val : freq) {
+           int dist = 0, curr = val;
+           while (curr > 0 && seen.contains(curr)) {
+               dist++;
+               curr--;
            } //while
+           sol += dist;
+           seen.add(curr);
        } //for
 
         return sol;
     } //minDeletions
-
-    private int searchEmpty(int i, int [] buckets) {
-        for (int j = i - 1; j >= 0; j--) {
-            if (buckets[j] == 0) return j;
-        } //for
-        return 0;
-    } //searchEmpty
 } //Sol
