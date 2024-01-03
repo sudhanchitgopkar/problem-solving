48c7f120a0740973b6c98a8d63e207c7399bb10f
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-01-02 20:39:26 -0500

    Time: 163 ms (35.99%) | Memory: 19.4 MB (17.25%) - LeetSync

--- LeetCode/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
+++ LeetCode/2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.py
@@ -3,9 +3,10 @@ class Solution:
         sol, prev_devices = 0, 0
 
         for row in bank:
-            curr_devices = sum([int(x) for x in row])
+            curr_devices = row.count('1')
             sol += curr_devices * prev_devices
-            prev_devices = curr_devices if curr_devices != 0 else prev_devices
+            if curr_devices != 0:
+                prev_devices = curr_devices 
         
         return sol
         
