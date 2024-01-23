b9bfd14b59c9e13aac73ba6977ccbb518d1762ea
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-01-22 20:11:47 -0500

    Time: 12 ms (67.34%) | Memory: 44.3 MB (60.69%) - LeetSync

--- LeetCode/1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ LeetCode/1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -4,26 +4,30 @@ class Solution {
     } //maxLen
 
     int backtrack(String curr, int i, List<String> arr) {
+        //if we've tried every string
         if (i == arr.size()) return curr.length();
         
         boolean valid = true;
         HashSet<Character> seen = new HashSet <> ();
-        for (int j = 0; j < arr.get(i).length(); j++) {
-            if (curr.contains(arr.get(i).charAt(j)+"") || 
+
+        for (int j = 0; j < arr.get(i).length() && valid; j++) {
+            //if this is in curr or a duplicate char within i
+            if (curr.contains(arr.get(i).substring(j, j + 1)) || 
                 seen.contains(arr.get(i).charAt(j))) {
                 valid = false;
-                break;
             } else {
                 seen.add(arr.get(i).charAt(j));
             } //if
         } //for 
 
         if (valid) {
+            //what's better? using this string or not?
             int without = backtrack(curr, i + 1, arr);
             curr += arr.get(i);
             int with = backtrack(curr, i + 1, arr);            
             return Math.max(with, without);
         } else {
+            //we don't have a choice, can't use this string
             return backtrack(curr, i + 1, arr);
         } //if
     } //backtrack
