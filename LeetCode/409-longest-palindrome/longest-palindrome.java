9baa0dc713b5327d7d5e26961997ff1661d9eeec
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-06-04 19:38:54 -0400

    Time: 7 ms (53.72%) | Memory: 41.6 MB (71.81%) - LeetSync

--- LeetCode/409-longest-palindrome/longest-palindrome.java
+++ LeetCode/409-longest-palindrome/longest-palindrome.java
@@ -5,7 +5,6 @@ class Solution {
 
         for (char c : s.toCharArray()) freq.put(c, freq.getOrDefault(c, 0) + 1);
         for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
-            System.out.println(entry.getKey() + "\t" + entry.getValue());
             sol += entry.getValue() - (entry.getValue() % 2 == 0 ? 0 : 1);
             if (entry.getValue() % 2 == 1) hasOdd = 1;
         } //for
