86d5d3518bdd2e5042c2f0163ea2c1031d8f32cf
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-01 21:53:21 -0400

    Time: 0 ms (100.00%) | Memory: 46.3 MB (5.92%) - LeetSync

--- LeetCode/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ LeetCode/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) return new int [] {-1, -1};
-        int first = findFirst(nums, target);
-        if (first == -1) return new int [] {-1, -1};
-        int last = findLast(nums, first, target);
-        return new int [] {first, last};
+        return new int [] {findFirst(nums, target), findLast(nums, target)};
     } //searchRange
 
-    private int findFirst(int [] n, int t) {
-        int l = 0, r = n.length - 1, sol = -1;
+    private int findFirst (int [] nums, int target) {
+        int l = 0, r = nums.length - 1, sol = -1;
+
         while (l <= r) {
             int m = l + (r - l)/2;
 
-            if (n[m] > t) {
+            if (nums[m] == target) {
+                sol = m;
                 r = --m;
-            } else if (n[m] < t) {
+            } else if (nums[m] < target) {
                 l = ++m;
             } else {
-                sol = m;
                 r = --m;
             } //if
         } //while
@@ -25,22 +22,22 @@ class Solution {
         return sol;
     } //findFirst
 
-    private int findLast(int [] n, int l, int t) {
-        int r = n.length - 1, sol = -1;
+    private int findLast (int [] nums, int target) {
+        int l = 0, r = nums.length - 1, sol = -1;
 
         while (l <= r) {
             int m = l + (r - l)/2;
 
-            if (n[m] > t) {
-                r = --m;
-            } else if (n[m] < t) {
-                l = ++m;
-            } else {
+            if (nums[m] == target) {
                 sol = m;
                 l = ++m;
+            } else if (nums[m] < target) {
+                l = ++m;
+            } else {
+                r = --m;
             } //if
         } //while
 
         return sol;
-    } //findLast
+    } //findFirst
 } //Sol
