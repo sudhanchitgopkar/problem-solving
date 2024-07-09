19f86dd07a14cfa438e9fb5c31277b0dc85f41e0
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-08 21:15:32 -0400

    Time: 3 ms (93.06%) | Memory: 88.6 MB (5.31%) - LeetSync

--- LeetCode/1803-average-waiting-time/average-waiting-time.java
+++ LeetCode/1803-average-waiting-time/average-waiting-time.java
@@ -5,13 +5,14 @@ class Solution {
         double waitTimeSum = 0;
 
         finishTimes[0] = customers[0][0] + customers[0][1];
-        waitTimeSum += finishTimes[0] - customers[0][0];
         for (int i = 1; i < n; i++) {
             finishTimes[i] = (finishTimes[i - 1] >= customers[i][0]) ?
             finishTimes[i - 1] + customers[i][1] : customers[i][0] + customers[i][1];
-             waitTimeSum += finishTimes[i] - customers[i][0];
         } //for
 
+        for (int i = 0; i < n; i++) {
+            waitTimeSum += finishTimes[i] - customers[i][0];
+        } //for
 
         return waitTimeSum/n;
     } //avgWaitingTime
