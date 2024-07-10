e35ae3ef274ae3b915a1d0606b129d91f8e39ed9
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-09 21:24:12 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (26.85%) - LeetSync

--- LeetCode/1720-crawler-log-folder/crawler-log-folder.java
+++ LeetCode/1720-crawler-log-folder/crawler-log-folder.java
@@ -1,12 +1,12 @@
 class Solution {
     public int minOperations(String[] logs) {
-        int depth = 0;
-
-        for (String log : logs) {
-            if (log.equals("../")) depth = Math.max(depth - 1, 0);
-            else if (!log.equals("./")) ++depth;
-        } //for
-
-        return depth;
+        return solve(logs, 0, 0);
     } //minOps
+
+    private int solve(String [] logs, int i, int depth) {
+        if (i == logs.length) return depth;
+        if (logs[i].equals("../")) return solve(logs, i + 1, depth == 0 ? 0 : depth - 1);
+        if (logs[i].equals("./")) return solve(logs, i + 1, depth);
+        return solve(logs, i + 1, depth + 1);
+    } //solve
 } //Sol
