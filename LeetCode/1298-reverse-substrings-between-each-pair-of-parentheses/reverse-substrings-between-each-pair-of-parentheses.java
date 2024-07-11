8bad0b047d99e1984f94a319dba2d988bfcb7373
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-10 21:36:10 -0400

    Time: 7 ms (21.40%) | Memory: 43.4 MB (15.75%) - LeetSync

--- LeetCode/1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
+++ LeetCode/1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -2,19 +2,19 @@ class Solution {
     public String reverseParentheses(String s) {
         Stack<Integer> openParen = new Stack <> ();
 
-        //find all sets of parens
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') openParen.push(i);
             if (s.charAt(i) == ')') {
                 s = boundedRev(s, openParen.pop(), i);
                 i -= 2;
-            }
+            } //if
         } //for
+        
         return s;
     } //revParen
 
     private String boundedRev(String s, int l, int r) {
-        //reverses s between indices [l, r]
+        //reverses s between indices (l, r), deleting l and r
         String pre = s.substring(0, l);
         String suf = s.substring(r + 1);
         String rev = new StringBuilder(s.substring(l + 1, r)).reverse().toString();
