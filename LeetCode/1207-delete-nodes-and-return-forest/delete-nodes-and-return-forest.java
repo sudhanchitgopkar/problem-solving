fbfa6e17656404a6107f570106599ea3fdbe31ed
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-16 21:53:30 -0400

    Time: 2 ms (27.75%) | Memory: 45.1 MB (5.60%) - LeetSync

---
+++ LeetCode/1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.java
@@ -0,0 +1,50 @@
+class Solution {
+    TreeNode [] nodeRefs;
+    TreeNode [] parents;
+    public List<TreeNode> delNodes(TreeNode root, int[] toDel) {
+        List<TreeNode> sol = new ArrayList <> ();
+        nodeRefs = new TreeNode [10001];
+        parents = new TreeNode [10001];
+        initRefs(root);
+
+        sol.add(root);
+        for (int del : toDel) {
+            TreeNode curr = nodeRefs[del];
+            TreeNode parent = parents[del];
+            if (curr.left != null) sol.add(curr.left);
+            if (curr.right != null) sol.add(curr.right);
+
+            if (parent != null && parent.left == curr) parent.left = null;
+            else if (parent != null) parent.right = null;
+        } //for
+
+        for (int del : toDel) {
+            int delIndex = -1;
+            for (int i = 0; i < sol.size(); i++) {
+                if (sol.get(i).val == del) {
+                    delIndex = i;
+                    break;
+                } //if
+                
+            } //for
+            if (delIndex != -1) {
+                sol.remove(delIndex);
+            } //if
+        } //for
+
+        return sol;
+    } //delNodes
+
+    private void initRefs(TreeNode root) {
+        nodeRefs[root.val] = root;
+        if (root.left != null) {
+            parents[root.left.val] = root;
+            initRefs(root.left);
+        } //if
+
+        if (root.right != null) {
+            parents[root.right.val] = root;
+            initRefs(root.right);
+        } //if
+    } //initRefs
+} //Sol
