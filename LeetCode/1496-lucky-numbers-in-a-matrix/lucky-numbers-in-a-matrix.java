d922c6941c519aaf40200713114fe7a4d7ee6411
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-18 21:17:19 -0400

    Time: 4 ms (24.97%) | Memory: 45.5 MB (10.55%) - LeetSync

--- LeetCode/1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
+++ LeetCode/1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -8,18 +8,14 @@ class Solution {
         //1. find the min of each row
         for (int i = 0; i < m; i++) {
             int min = Integer.MAX_VALUE;
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] < min) min = matrix[i][j];
-            } //for
+            for (int j = 0; j < n; j++) min = Math.min(min, matrix[i][j]);
             rowMins[i] = min;
         } //for
 
         //2. find the max of each col
         for (int i = 0; i < n; i++) {
             int max = 0;
-            for (int j = 0; j < m; j++) {
-                if (matrix[j][i] > max) max = matrix[j][i];
-            } //for
+            for (int j = 0; j < m; j++) max = Math.max(max, matrix[j][i]);
             colMaxs.add(max);
         } //for
 
