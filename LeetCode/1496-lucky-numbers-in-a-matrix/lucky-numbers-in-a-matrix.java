34405dc28da23cf154f3615bae469eb042220065
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-18 21:14:59 -0400

    Time: 3 ms (42.69%) | Memory: 45.3 MB (32.97%) - LeetSync

---
+++ LeetCode/1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int [] rowMins = new int [m];
+        HashSet <Integer> colMaxs = new HashSet <> ();
+        List <Integer> sol = new ArrayList <> ();
+        
+        //1. find the min of each row
+        for (int i = 0; i < m; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] < min) min = matrix[i][j];
+            } //for
+            rowMins[i] = min;
+        } //for
+
+        //2. find the max of each col
+        for (int i = 0; i < n; i++) {
+            int max = 0;
+            for (int j = 0; j < m; j++) {
+                if (matrix[j][i] > max) max = matrix[j][i];
+            } //for
+            colMaxs.add(max);
+        } //for
+
+        for (int i = 0; i < m; i++) {
+            if (colMaxs.contains(rowMins[i])) sol.add(rowMins[i]);
+        } //for
+
+        return sol;
+    } //luckyNums
+} //Sol
+
+/*
+    03 07 08 - 03
+    09 11 13 - 09
+    15 16 17 - 15
+    |  |  |
+    15 16 17
+*/
