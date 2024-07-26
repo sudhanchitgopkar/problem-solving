1f45cb1e95b5b57e164db43454a02bb605187f39
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-25 21:39:36 -0400

    Time: 312 ms (5.23%) | Memory: 45.8 MB (5.54%) - LeetSync

---
+++ LeetCode/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -0,0 +1,51 @@
+class Solution {
+    HashMap <Integer, List<int []>> adj;
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int minNeighbors = Integer.MAX_VALUE, cityID = -1;
+        adj = new HashMap <> ();
+
+        //populate adj
+        for (int i = 0; i < n; i++) adj.put(i, new ArrayList<int[]> ());
+
+        for (int [] edge : edges) {
+            adj.get(edge[0]).add(new int [] {edge[1], edge[2]});
+            adj.get(edge[1]).add(new int [] {edge[0], edge[2]});
+        } //for
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + ":");
+            for (int [] edge : adj.get(i)) {
+                System.out.print("[" + edge[0] + "," + edge[1] + "]");
+            } //for
+            System.out.println();
+        } //for
+
+        for (int i = 0; i < n; i++) {
+            int [] seen = new int [n];
+            for (int j = 0; j < n; j++) seen[j] = Integer.MIN_VALUE;
+            seen[i] = Integer.MAX_VALUE;
+            int neighbors = countNeighbors(i, distanceThreshold, seen);
+            System.out.println(i + "->" + neighbors);
+            if (neighbors <= minNeighbors) {
+                minNeighbors = neighbors;
+                cityID = i;
+            } //if
+        } //for
+
+        return cityID;
+    } //findTheCity
+
+    private int countNeighbors(int curr, int distLeft, int [] seen) {
+        for (int [] next : adj.get(curr)) {
+            int loc = next[0], dist = next[1];
+            if (seen[loc] < (distLeft - dist) && dist <= distLeft) {
+                seen[loc] = distLeft - dist;
+                countNeighbors(loc, distLeft - dist, seen);
+            } //if
+        } //for
+
+        int sol = 0;
+        for (int city : seen) if (city >= 0) ++sol;
+        return sol;
+    } //countNeighbors
+} //Sol
