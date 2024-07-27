005252af8357b38ba02e34b9e7d38a89e4ad902e
Sudhan Chitgopkar <49770077+sudhanchitgopkar@users.noreply.github.com>
2024-07-26 21:30:29 -0400

    Time: 3881 ms (7.62%) | Memory: 18.7 MB (10.66%) - LeetSync

---
+++ LeetCode/3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        adj = collections.defaultdict(list)
+        cache = {}
+
+        #Create adj list
+        edges = zip(original, changed, cost)
+        for o, ch, co in edges:
+            adj[o].append((co, ch))
+
+        def dijk(src, dst):
+            if (src, dst) in cache:
+                return cache[(src, dst)]
+            pq = [(0, src)]
+            visited = set()
+            
+            while pq:
+                cost, node = heapq.heappop(pq)
+                visited.add(node)
+                
+
+                if node == dst:
+                    cache[(src, node)] = cost
+                    return cost
+                
+                for nxt_cost, nxt_node in adj[node]:
+                    if nxt_node not in visited:
+                        heapq.heappush(pq, (cost + nxt_cost, nxt_node))
+
+            return -1
+        
+        sol = 0
+        for char_src, char_dst in zip(source, target):
+            res = dijk(char_src, char_dst)
+            if res == -1:
+                return -1
+            sol += res
+        
+        return sol
+
+
+
+
+        
+
+        
